519ad04690707d504b98b14206747eee239cc899
TJ Maynes <tj@tjmaynes.com>
2024-06-17 23:23:01 -0500

    chore: first commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import './App.css'
+
+const JsonValidator = () => {
+    return (
+        <div>
+            JSON Validator
+        </div>
+    )
+}
+
+const App = () => {
+  return (
+    <>
+        <JsonValidator />
+    </>
+  )
+}
+
+export default App
